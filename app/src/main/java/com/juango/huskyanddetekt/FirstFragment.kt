feda98ceda566998f063f0151d4077321cb5a08a
juango <jgonzalofh2898@gmail.com>
2021-08-12 23:23:17 -0500

    Applying change from detekt

--- app/src/main/java/com/juango/huskyanddetekt/FirstFragment.kt
+++ app/src/main/java/com/juango/huskyanddetekt/FirstFragment.kt
@@ -20,13 +20,12 @@ class FirstFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/com/juango/huskyanddetekt/SecondFragment.kt
+++ app/src/main/java/com/juango/huskyanddetekt/SecondFragment.kt
@@ -20,13 +20,12 @@ class SecondFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
